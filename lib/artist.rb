e06c18007e5eff1d9868d1c7e6fbbdb782d5d869
juddB <Juddbahoui@gmail.com>
2021-02-10 18:39:01 -0500

    commit tree cd1fd0c21a5536d515c5576575352bd47764f9d3

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist
+
+attr_accessor :name, :songs
+
+@@all = []
+
+
+def initialize(name)
+    @name = name 
+    @@all << self
+end
+
+def self.all
+    @@all 
+end
+
+def songs
+    Song.all.select do |song| 
+    song.artist == self
+    end
+end
+
+def add_song(songs)
+    songs.artist = self
+end
+
+def self.find_or_create_by_name(name)
+ self.all.detect{ |artist| artist.name == name }  || Artist.new(name)
+end
+
+def print_songs
+self.songs.each { |song| puts song.name }
+end
+
+
+
+
+
+
+end
